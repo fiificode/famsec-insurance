8cc3b1c1f5a2f3cdfeca244411cfa61de7e301f8
Franklin Mensah <fiificode@gmail.com>
2023-11-16 16:58:22 +0000

    Added stats section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
-import Image from 'next/image'
-import HeroSection from './components/Hero'
+import Image from "next/image";
+import HeroSection from "./components/Hero";
+import Stats from "./components/Stats";
 
 export default function Home() {
   return (
-  <HeroSection />
-  )
+    <>
+      <HeroSection />
+      <Stats />
+    </>
+  );
 }
