2b87daf9b464f8499cb9c4f3b16d32671675b7a6
Franklin Mensah <fiificode@gmail.com>
2023-11-16 18:12:44 +0000

    Added Services section

---
+++ app/components/Services.jsx
@@ -0,0 +1,110 @@
+import Image from "next/image";
+import React from "react";
+
+const servicesContent = {
+  heading: {
+    headingSubTitle: "Our Awesome Services",
+    headingTitle: "Our Services",
+    description:
+      "We understand that protecting your loved ones is of utmost importance. That's why we are dedicated to providing comprehensive solutions tailored to your family's unique needs.",
+  },
+  items: [
+    {
+      icon: "/images/icon-1.svg",
+      title: "Child Safety Solutions",
+      description:
+        "Childproofing services and safety education to keep your children secure and protected at home and in public spaces.",
+    },
+    {
+      icon: "/images/icon-2.svg",
+      title: "Elderly Monitoring",
+      description:
+        "Innovative technology for monitoring and caring for elderly family members,promoting their safety and independence.",
+    },
+    {
+      icon: "/images/icon-3.svg",
+      title: "Home Fire Safety",
+      description:
+        "Childproofing services and safety education to keep your children secure and protected at home and in public spaces.",
+    },
+    {
+      icon: "/images/icon-4.svg",
+      title: "Financial Planning",
+      description:
+        "Expert financial advice to help you build a strong financial foundation and plan for your family's long-term security.",
+    },
+    {
+      icon: "/images/icon-5.svg",
+      title: "Personal Safety Devices",
+      description:
+        "Wearable personal safety devices equipped with GPS tracking and emergency alerts for family members on the go.",
+    },
+    {
+      icon: "/images/icon-6.svg",
+      title: "Insurance Coverage",
+      description:
+        "Tailored insurance policies to protect your family's financial future,including life insurance,health insurance and property insurance.",
+    },
+  ],
+};
+
+const Services = () => {
+  return (
+    <section className="py-20 bg-light">
+      <div className="container px-4 mx-auto">
+        <div className="max-w-xl mx-auto text-center mb-20">
+          {servicesContent?.heading?.headingSubTitle && (
+            <span
+              className="inline-block py-0.5 z-50 pl-3 text-heading font-semibold relative 
+                    mb-7 before:content-[''] before:absolute before:w-2/3 before:bg-yellowLight 
+                    before:left-0 before:top-0 before:bottom-0 before:z-[-1]"
+            >
+              {servicesContent?.heading.headingSubTitle}
+            </span>
+          )}
+          {servicesContent?.heading?.headingTitle && (
+            <h2 className="text-heading text-2xl lg:text-4xl font-bold">
+              {servicesContent.heading.headingTitle}
+            </h2>
+          )}
+          {servicesContent?.heading?.description && (
+            <p className="text-body leading-relaxed">
+              {servicesContent.heading.description}
+            </p>
+          )}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
+            {servicesContent?.items.map((item,idx) => (
+                <div key={item.title} className="flex space-x-10">
+                    <div className="w-14 shrink-0">
+                        <span className="inline-flex items-center justify-center p-2 w-[70px] h-[70px] rounded-lg bg-white shadow-2xl">
+                            <Image 
+                                src={item.icon}
+                                width={62}
+                                height={62}
+                                alt="icons"
+                            />
+                        </span>
+                    </div>
+                    <div>
+                        {item?.title && (
+                            <h3 className="text-heading font-bold text-md">
+                                {item.title}
+                            </h3>
+                        )}
+                          {item?.description && (
+                            <h3 className="leading-relaxed text-sm">
+                                {item.description}
+                            </h3>
+                        )}
+                    </div>
+                </div>
+                
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/Hero";
 import Stats from "./components/Stats";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Stats />
+      <Services />
     </>
   );
 }
